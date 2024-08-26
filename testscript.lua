48c4e0ffc9289dd02845008f6cb7c96406b40b54
TheG450 <brfxxx450@gmail.com>
2024-08-27 02:30:03 +0700

    update and add

---
+++ testscript.lua
@@ -0,0 +1,18 @@
+local workspace = game:GetService("Workspace")
+local player = game.Players.LocalPlayer
+local character = player.Character
+
+for _, v in pairs(workspace.Npcs["DarkPiratesBoss"]:GetDescendants()) do
+    if v:IsA("Humanoid") then
+        if v.Health > 0 then
+            for _, Target in pairs(workspace.Npcs["DarkPiratesBoss"]:GetChildren()) do
+                if Target.Name == "HumanoidRootPart" then
+                    character.HumanoidRootPart.CFrame = Target.CFrame
+                end
+            end
+        end
+        if v.Health < v.MaxHealth then
+            v.Health = -899999999
+        end
+    end
+end
