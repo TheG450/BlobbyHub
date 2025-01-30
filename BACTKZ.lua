efe01b381cb21c8a78b70f3e58f8e83f3d56137e
TheG450 <brfxxx450@gmail.com>
2025-01-30 23:37:47 +0700

    Update Fix All Bug

--- BACTKZ.lua
+++ BACTKZ.lua
@@ -8,6 +8,7 @@ getgenv().Settings = {
     Speed = nil,
     DeliveryPoint = nil,
     DeliveryPointExtra = nil,
+    AutoFarm = nil,
 }
 local AntiSpam = false
 
@@ -72,16 +73,16 @@ do
             
                 local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Distance/Speed), {CFrame = Target})
                 if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("antifall") and game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChildOfClass("BodyVelocity") then
-                    local TweenHeight = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,50,0)})
+                    local TweenHeight = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1), {CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,50,0)})
                     TweenHeight:Play()
                     TweenHeight.Completed:Connect(function()
                         Tween:Play()
                         Tween.Completed:Connect(function()
                             game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-                            task.wait(1)
                             pcall(function()
                                 game:GetService("ReplicatedStorage").RemoteEvents.DeliveryHandlerRemotes.DeliverItems:InvokeServer(getgenv().Settings.DeliveryPoint)
                             end)
+                            task.wait(1)
                             for i,v in pairs(game.Players.LocalPlayer.Character.HumanoidRootPart:GetChildren()) do
                                 if v.Name == "antifall" then
                                     v:Destroy()
@@ -104,6 +105,23 @@ do
             pcall(function()
                 if not AntiSpam then
                     AntiSpam = true
+                    local function TeleportPoint()
+                        for x=1, 2 do
+                            if getgenv().Settings.DeliveryPoint == "1" then
+                                TP(CFrame.new(-15.5, 461, -1221.5))
+                            elseif getgenv().Settings.DeliveryPoint == "2" then
+                                TP(CFrame.new(-1018.5, 462.5, -1221.5))
+                            elseif getgenv().Settings.DeliveryPoint == "3" then
+                                TP(CFrame.new(-2121.5, 462.5, -2134.5))
+                            elseif getgenv().Settings.DeliveryPoint == "4" then
+                                TP(CFrame.new(-531.75, 462.5, -3354.5))
+                            elseif getgenv().Settings.DeliveryPoint == "5" then
+                                TP(CFrame.new(-310.5, 462.5, -5784.5))
+                            elseif getgenv().Settings.DeliveryPoint == "6" then
+                                TP(CFrame.new(-310.5, 462.5, -7339.5))
+                            end
+                        end
+                    end
                     for i,v in pairs(game:GetService("Workspace").BuildZones:GetChildren()) do
                         if v.Name == "Zone" and v.Player.Value == game.Players.LocalPlayer then
                         local Vehicle = v:FindFirstChild("Vehicle") or v:WaitForChild("Vehicle", 9e99)
@@ -119,21 +137,7 @@ do
                                         end
                                     end
                                 end
-                                for x=1, 2 do
-                                    if getgenv().Settings.DeliveryPoint == "1" then
-                                        TP(CFrame.new(-15.5, 461, -1221.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "2" then
-                                        TP(CFrame.new(-1018.5, 462.5, -1221.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "3" then
-                                        TP(CFrame.new(-2121.5, 462.5, -2134.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "4" then
-                                        TP(CFrame.new(-531.75, 462.5, -3354.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "5" then
-                                        TP(CFrame.new(-310.5, 462.5, -5784.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "6" then
-                                        TP(CFrame.new(-310.5, 462.5, -7339.5))
-                                    end
-                                end
+                                TeleportPoint()
                             else
                                 game:GetService("ReplicatedStorage").Shared.DeployHandler.AssembleCar:InvokeServer()
                                 wait(1.5)
@@ -149,21 +153,12 @@ do
                                         end
                                     end
                                 end
-                                for x=1, 2 do
-                                    if getgenv().Settings.DeliveryPoint == "1" then
-                                        TP(CFrame.new(-15.5, 461, -1221.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "2" then
-                                        TP(CFrame.new(-1018.5, 462.5, -1221.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "3" then
-                                        TP(CFrame.new(-2121.5, 462.5, -2134.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "4" then
-                                        TP(CFrame.new(-531.75, 462.5, -3354.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "5" then
-                                        TP(CFrame.new(-310.5, 462.5, -5784.5))
-                                    elseif getgenv().Settings.DeliveryPoint == "6" then
-                                        TP(CFrame.new(-310.5, 462.5, -7339.5))
+                                for BodyIndex, BodyValue in pairs(CarModel:GetChildren()) do
+                                    if BodyValue.Name ~= "VehicleSeat" and BodyValue:IsA("Model") then
+                                        BodyValue:Destroy()
                                     end
                                 end
+                                TeleportPoint()
                                 
                             end
                         end
@@ -178,7 +173,155 @@ do
             end)
         end
     })
+    local Start = false
+    local Spawn = false
+    local AutoFarm = Tabs.pageMain:AddToggle("AutoFarm", {Title = "Auto Farm", Default = getgenv().Settings.AutoFarm or false })
+    -- local FixBug = Tabs.pageMain:AddButton({
+    --     Title = "FixBug",
+    --     Callback = function()
+    --         Spawn = false
+    --         Start = false
+    --     end
+    -- })
+
+    --[[ SCRIPTS ]]--------------------------------------------------------
+    
+    AutoFarm:OnChanged(function()
+        task.spawn(function()
+            local function Notify(Content, Duration)
+                Fluent:Notify({
+                    Title = "BlobbyHub",
+                    Content = Content,
+                    Duration = Duration
+                })
+            end
+            local function TP(Target)
+                local antifall;
+                local TweenService = game:GetService("TweenService")
+                local Distance = (Target.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+                local Speed = getgenv().Settings.Speed
+            
+                local Tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Distance/Speed, Enum.EasingStyle.Linear), {CFrame = Target})
+                if not AutoFarm.Value then
+                    Tween:Cancel()
+                end
+                if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("antifall") and game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChildOfClass("BodyVelocity") then
+                    local TweenHeight = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear), {CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,100,0)})
+                    TweenHeight:Play()
+                    TweenHeight.Completed:Connect(function()
+                        Tween:Play()
+                        Tween.Completed:Connect(function()
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+                            pcall(function()
+                                game:GetService("ReplicatedStorage").RemoteEvents.DeliveryHandlerRemotes.DeliverItems:InvokeServer(getgenv().Settings.DeliveryPoint)
+                            end)
+                            task.wait(1)
+                            for i,v in pairs(game.Players.LocalPlayer.Character.HumanoidRootPart:GetChildren()) do
+                                if v.Name == "antifall" then
+                                    v:Destroy()
+                                end
+                            end
+                            wait(2)
+                            Notify("Delivery Success", 2)
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+                            game:GetService("ReplicatedStorage").Shared.DeployHandler.ReturnHome:FireServer()
+                            wait(2)
+                            Spawn = false
+                            Start = false
+                        end)
+                    end)
+                else
+                    antifall = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
+                    antifall.Velocity = Vector3.new(0, 0, 0)
+                    antifall.MaxForce = Vector3.new(100000, 100000, 100000)
+                    antifall.P = 1250 --1250
+                    antifall.Name = "antifall"
+                end
+            end
+            local function TeleportPoint()
+                for x=1, 2 do
+                    if getgenv().Settings.DeliveryPoint == "1" then
+                        TP(CFrame.new(-15.5, 461, -1221.5))
+                    elseif getgenv().Settings.DeliveryPoint == "2" then
+                        TP(CFrame.new(-1018.5, 462.5, -1221.5))
+                    elseif getgenv().Settings.DeliveryPoint == "3" then
+                        TP(CFrame.new(-2121.5, 462.5, -2134.5))
+                    elseif getgenv().Settings.DeliveryPoint == "4" then
+                        TP(CFrame.new(-531.75, 462.5, -3354.5))
+                    elseif getgenv().Settings.DeliveryPoint == "5" then
+                        TP(CFrame.new(-310.5, 462.5, -5784.5))
+                    elseif getgenv().Settings.DeliveryPoint == "6" then
+                        TP(CFrame.new(-310.5, 462.5, -7339.5))
+                    end
+                end
+            end
+            while AutoFarm.Value do
+                wait()
+                for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
+                    if v.Name == "Trees" or v.Name == "Towers" or v.Name == "DesertHouses" or v.Name == "IceSpikes" or v.Name == "Barricades" or v.Name == "Mountian" or v.Name == "Rocks" or v.Name == "Roadblocks" or v.Name == "IceRoadBlockers" or v.Name == "SnowHills" or v.Name == "BreakingPlatforms" or v.Name == "ForestHills" or v.Name == "ContainersOnRoad" or v.Name == "Containers" or v.Name == "CaveRocks" or v.Name == "IceBridge" or v.Name == "IceWall" or v.Name == "DesertRoadBlocker" then
+                        v:Destroy()
+                    end
+                end
+                for i,v in pairs(game:GetService("Workspace").BuildZones:GetChildren()) do
+                    if v.Name == "Zone" and v.Player.Value == game.Players.LocalPlayer then
+                    local Vehicle = v:FindFirstChild("Vehicle") or v:WaitForChild("Vehicle", 9e99)
+                    local Count = Vehicle:GetChildren()
+                        if #Count == 1 then
+                            local CarModel = Vehicle:FindFirstChild("CarModel")
+                            for CrateIndex, CrateValue in pairs(CarModel:GetChildren()) do
+                                if string.find(CrateValue.Name, "Crate") or string.find(CrateValue.Name, "FirstAidKit") or string.find(CrateValue.Name, "WheatPalette") then
+                                    if not Start then
+                                        Start = true
+                                        for WheelsIndex, WheelsValue in pairs(CarModel:GetChildren()) do
+                                            if WheelsValue.Name == "Wheels" and WheelsValue:IsA("Folder") then
+                                                for WheelIndex, WheelValue in pairs(WheelsValue:GetChildren()) do
+                                                    if WheelValue:IsA("BasePart") then
+                                                        WheelValue:Destroy()
+                                                    end
+                                                end
+                                            end
+                                        end
+                                        for BodyIndex, BodyValue in pairs(CarModel:GetChildren()) do
+                                            if BodyValue.Name ~= "VehicleSeat" and BodyValue:IsA("Model") then
+                                                BodyValue:Destroy()
+                                            end
+                                        end
+                                        TeleportPoint()
+                                        Notify("Started Delivery", 2)
+                                    end
+                                end
+                            end
+                        else
+                            if not Spawn then
+                                Spawn = true
+                                game:GetService("ReplicatedStorage").Shared.DeployHandler.AssembleCar:InvokeServer()
+                                wait(1.5)
+                                Notify("Deploying Car", 2)
+                                game:GetService("ReplicatedStorage").Shared.DeployHandler.DeployCar:FireServer()
+                                wait(.5)
+                            end
+                        end
+                    end
+                end
+            end
+        end)
+    end)
+
 end
+
+-- Anti AFK
+task.spawn(function()
+    while wait(320) do
+        pcall(function()
+            local anti = game:GetService("VirtualUser")
+            game:GetService("Players").LocalPlayer.Idled:connect(function()
+                anti:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+                wait(1)
+                anti:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+            end)
+        end)
+    end
+end)
 
 Window:SelectTab(1)
 
